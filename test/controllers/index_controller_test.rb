75eeeed277a522d4f60edd40ab86d9d570b971a0
Philip Kiely <kielyphi@grinnell.edu>
2019-10-02 21:48:15 -0500

    HW6

--- test/controllers/index_controller_test.rb
+++ test/controllers/index_controller_test.rb
@@ -4,5 +4,9 @@ class IndexControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get '/'
     assert_response :success
+    assert_not_nil assigns(:income)
+    assert_not_nil assigns(:hours)
+    assert_not_nil assigns(:admin)
+    assert_not_nil assigns(:rate)
   end
 end
