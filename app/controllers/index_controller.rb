9e805565ed9020a2644aa096411e855c03286336
Philip Kiely <kielyphi@grinnell.edu>
2019-10-30 20:44:47 -0500

    Forms HW 8

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,4 +4,9 @@ class IndexController < ApplicationController
       @title = "Income Tracker Application"
       @invoices = Invoice.all
   end
+  
+  def new
+      Invoice.create!({"income": params["income"].to_i, "hours": params["hours"].to_i, "admin": params["admin"].to_i})
+      redirect_to '/'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/about/', to: 'about#show'
   get '/pricing/', to: 'pricing#show'
+  post '/new/', to: 'index#new'
   root 'index#show'
 end
